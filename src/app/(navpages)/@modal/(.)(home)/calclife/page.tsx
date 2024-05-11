4f63ebf47e98d9633cec44e0b06128a8178c1d53
optshj <optshj@gmail.com>
2024-05-11 23:32:04 +0900

    [Feat] 모달 클릭시 뒤로가기 구현

--- src/app/(navpages)/@modal/(.)(home)/calclife/page.tsx
+++ src/app/(navpages)/@modal/(.)(home)/calclife/page.tsx
@@ -3,15 +3,12 @@ import styled from 'styled-components';
 import Modal from '../../../_components/Modal';
 
 const Wrapper = styled.div`
-    width:1000px;
-    height:100px;
-    background-color:red;
 `
 export default function CalcLife(){
     return(
         <Modal>
-        <Wrapper>
-        </Wrapper>
+            <Wrapper>
+            </Wrapper>
         </Modal>
     )
 }
--- src/app/(navpages)/_components/Modal.tsx
+++ src/app/(navpages)/_components/Modal.tsx
@@ -1,17 +1,26 @@
+'use client'
 import React, { ReactNode, useCallback } from 'react';
-import Router from 'next/router';
+import { useRouter } from 'next/navigation';
 import styled from 'styled-components';
 
 const Outer = styled.div`
-    
+    position: absolute;
+    top:0;
+    z-index:101;
+    width:100%;
+    height:100%;
+    background-color:rgba(0, 0, 0, 0.8);
 `
 const Inner = styled.div`
+    width:500px;
+    height:200px;
+    color:white;
 `
 export default function Modal({children}: Readonly<{children:ReactNode}>){
-
+    const router = useRouter();
     const onClose = useCallback(() => {
-        Router.back();
-    },[Router])
+        router.back();
+    },[router])
 
     return(
         <Outer onClick={onClose}>
