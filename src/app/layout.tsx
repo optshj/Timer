9f6fa0039bdda611ddfb097a45d5e3a6019925d2
optshj <optshj@gmail.com>
2024-06-01 18:55:57 +0900

    [Feat] SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,23 @@
 import RootStyleRegistry from "../_lib/registy";
 import DarkmodeScript from "../_script/DarkmodeScript";
 import ClientApp from "./ClientApp";
-import SEO from "./SEO";
 
 export default function RootLayout({children}: Readonly<{children: React.ReactNode}>) {
   return (
     <html lang="en">
       <head>
+      <title>LifeTimer</title>
+            <meta name="description" content="나의 남은 수명을 계산해보고, 건강관련 팁을 받아가세요."/>
+            <meta property="og:type" content="website" />
+            <meta property="og:description" content="앞으로 얼마나 살 수 있을지 눈으로 확인해보세요"/>
+            <meta property="og:url" content="https://lifetimer1.vercel.app/"/>
+            <meta property="og:locale" content="ko_KR" />
+
+            <link rel="canonical" href="https://lifetimer1.vercel.app/"/>
+            <link rel="alternate" href="https://lifetimer1.vercel.app/" hrefLang="ko_KR"/>
+            <link rel="manifest" href="manifest url"/>
         <DarkmodeScript/>
       </head>
-      <SEO/>
       <RootStyleRegistry>
         <ClientApp>
           {children}
