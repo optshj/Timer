68450521318412cf5cb700b25250d84cfa941359
optshj <optshj@gmail.com>
2024-06-05 20:43:42 +0900

    [Fix] metadata수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,13 +22,15 @@ export default function RootLayout({children}: Readonly<{children: React.ReactNo
         <title>LifeTimer</title>
         <meta name="description" content="나의 남은 수명을 계산해보고, 건강관련 팁을 받아가세요."/>
         <meta name="robots" content="index,follow"/>
-
-        <meta property="og:title" content="LifeTimer" />
+        <meta name="title" content="LifeTimer"/>
+        <meta property="og:title" content="LifeTimer"/>
         <meta property='og:site_name' content="LifeTimer" />
         <meta property="og:type" content="website" />
-        <meta property="og:description" content="앞으로 얼마나 살 수 있을지 눈으로 확인해보세요"/>
+        <meta property="og:description" content="남은 수명을 계산해보세요."/>
         <meta property="og:url" content="https://lifetimer1.vercel.app/"/>
         <meta property="og:locale" content="ko-KR" />
+        <meta property="og:image" content="@/src/_asset/img/logo.jpg" />
+
         <link rel="canonical" href="https://lifetimer1.vercel.app/"/>
         <link rel="alternate" hrefLang="ko-KR" href="https://lifetimer1.vercel.app/" />
 
